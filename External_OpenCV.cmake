d3884a9631680807cc70c7fffc3b0a14e92c71c9
Adam Rankin <arankin@robarts.ca>
2016-03-28 22:11:16 -0400

    Enabling linux build of opencv

--- External_OpenCV.cmake
+++ External_OpenCV.cmake
@@ -15,6 +15,13 @@ ELSE()
     SET(ep_common_cxx_flags "${ep_common_cxx_flags} /D_VARIADIC_MAX=10")
   ENDIF()
 
+  IF( NOT WIN32 )
+    # Remove -std=c++11 from cxx flags, as this breaks opencv 2.3.1 build on linux
+    STRING(REPLACE "-std=c++11" "" ep_common_cxx_flags ${ep_common_cxx_flags})
+    # Turn off Qt build as it causes usleep compiler errors
+    SET(QT_ARG -DWITH_QTL:BOOL=OFF)
+  ENDIF()
+
   SET (OpenCV_SRC_DIR ${ep_dependency_DIR}/OpenCV CACHE INTERNAL "Path to store OpenCV contents.")
   SET (OpenCV_BIN_DIR ${ep_dependency_DIR}/OpenCV-bin CACHE INTERNAL "Path to store OpenCV contents.")
   ExternalProject_Add(OpenCV
