aa0923464d73c2fde4e3e25d5121c3ecf0071393
Adam Rankin <adam.rankin@gmail.com>
2016-05-25 15:38:57 -0400

    Adding configurable VTK version

--- External_VTK.cmake
+++ External_VTK.cmake
@@ -1,6 +1,6 @@
 IF(VTK_DIR)
   # VTK has been built already
-  FIND_PACKAGE(VTK 6.3.0 REQUIRED NO_MODULE PATHS ${VTK_DIR} NO_DEFAULT_PATH)
+  FIND_PACKAGE(VTK ${RobartsVTK_VTK_VERSION} REQUIRED NO_MODULE PATHS ${VTK_DIR} NO_DEFAULT_PATH)
   
   IF( NOT VTK_WRAP_PYTHON AND RobartsVTK_WRAP_PYTHON )
     MESSAGE(FATAL_ERROR "Python wrapping requested but VTK located at \"${VTK_DIR}\" was not built with python wrapping enabled.")
@@ -18,10 +18,17 @@ IF(VTK_DIR)
 ELSE(VTK_DIR)
   # VTK has not been built yet, so download and build it as an external project
 
-  SET(VTK_GIT_REPOSITORY "gitlab.kitware.com/vtk/vtk.git")
-  SET(VTK_GIT_TAG "master")
+  IF(${RobartsVTK_VTK_VERSION} VERSION_EQUAL "6.3.0")
+    SET(VTK_GIT_REPOSITORY "github.com/Slicer/VTK.git")
+    SET(VTK_GIT_TAG "fe92273888219edca422f3a308761ddcd2882e2b")
+    SET(VTK_GIT_PROTOCOL ${GIT_PROTOCOL})
+  ELSE
+    SET(VTK_GIT_REPOSITORY "gitlab.kitware.com/vtk/vtk.git")
+    SET(VTK_GIT_TAG "master")
+    SET(VTK_GIT_PROTOCOL "https")
+  ENDIF()
 
-  MESSAGE(STATUS "Downloading and building VTK from: ${GIT_PROTOCOL}://${VTK_GIT_REPOSITORY}")
+  MESSAGE(STATUS "Downloading and building VTK from: ${VTK_GIT_PROTOCOL}://${VTK_GIT_REPOSITORY}")
 
   IF( RobartsVTK_USE_QT )
     SET(VTK_VERSION_SPECIFIC_ARGS ${VTK_VERSION_SPECIFIC_ARGS}
@@ -47,7 +54,7 @@ ELSE(VTK_DIR)
     SOURCE_DIR "${RobartsVTK_VTK_SRC_DIR}"
     BINARY_DIR "${RobartsVTK_VTK_DIR}"
     #--Download step--------------
-    GIT_REPOSITORY "https://${VTK_GIT_REPOSITORY}"
+    GIT_REPOSITORY "${VTK_GIT_PROTOCOL}://${VTK_GIT_REPOSITORY}"
     GIT_TAG ${VTK_GIT_TAG}
     #--Configure step-------------
     CMAKE_ARGS 
