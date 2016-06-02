07d6893a9da0e808cb3869f670d5458e73028d96
Adam Rankin <arankin@robarts.ca>
2016-06-02 15:48:10 -0400

    Adding pkg-config for windows
    Correcting OpenCV cxx_flags export variable

--- External_OpenCV.cmake
+++ External_OpenCV.cmake
@@ -27,15 +27,6 @@ ELSE()
     SET(ep_common_cxx_flags "${ep_common_cxx_flags} /D_VARIADIC_MAX=10")
   ENDIF()
 
-  IF( NOT WIN32 )
-    # Remove -std=c++11 from cxx flags, as this breaks opencv 2.3.1 build on linux
-    STRING(REPLACE "-std=c++11" "" opencv_common_cxx_flags ${ep_common_cxx_flags})
-    # Turn off Qt build as it causes usleep compiler errors
-    SET(QT_ARG -DWITH_QTL:BOOL=OFF)
-  ELSE()
-    SET(opencv_common_cxx_flags ${ep_common_cxx_flags})
-  ENDIF()
-
   SET (OpenCV_SRC_DIR ${ep_dependency_DIR}/OpenCV CACHE INTERNAL "Path to store OpenCV contents.")
   SET (OpenCV_BIN_DIR ${ep_dependency_DIR}/OpenCV-bin CACHE INTERNAL "Path to store OpenCV contents.")
   ExternalProject_Add(OpenCV
@@ -49,7 +40,7 @@ ELSE()
     CMAKE_ARGS 
       ${ep_common_args}
       -DCMAKE_C_FLAGS=${ep_common_c_flags}
-      -DCMAKE_CXX_FLAGS=${opencv_common_cxx_flags}
+      -DCMAKE_CXX_FLAGS=${ep_common_cxx_flags}
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_TESTS:BOOL=OFF
       -DBUILD_DOCS:BOOL=OFF
